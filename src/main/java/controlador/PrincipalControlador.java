dfc9de566001fa60685f655dd326301437824357
kevinixd <49318227+kevinixd@users.noreply.github.com>
2019-07-29 19:58:44 -0600

    Imagenes y Controlador Principal
    Se crearon carpetas para las imagenes, se realizaron cambios en el controlador principal

--- src/main/java/controlador/PrincipalControlador.java
+++ src/main/java/controlador/PrincipalControlador.java
@@ -9,9 +9,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import vista.JfrmPrincipal;
+import vista.jPnlBebidas;
 import vista.jPnlBebidasFrias;
 import vista.jPnlComboIndividual;
-import vista.jPnlCombos;
+import vista.jPnlHamburguesas;
 
 /**
  *
@@ -20,77 +21,91 @@ import vista.jPnlCombos;
 public class PrincipalControlador implements ActionListener {
 
     public static JfrmPrincipal principal = new JfrmPrincipal();
-    //menu principal
-    private int estado = 0;
-    //panel de submentus
-    jPnlCombos combos = new jPnlCombos();
-    jPnlComboIndividual combosIndividual = new jPnlComboIndividual();
-    jPnlBebidasFrias bebidas = new jPnlBebidasFrias();
+    jPnlHamburguesas hamburguesa = new jPnlHamburguesas();
+    jPnlComboIndividual comboIndividual = new jPnlComboIndividual();
+    jPnlBebidas bebida = new jPnlBebidas();
+    jPnlBebidasFrias bebidaFria = new jPnlBebidasFrias();
 
     //Ruta para las imagenes
     String ruta = System.getProperty("user.dir");
 
     public PrincipalControlador(JfrmPrincipal principal) {
         this.principal = principal;
-        imagenes();
+        imgPrincipal();
         principal.jBtnCombos.addActionListener(this);
         principal.jBtnBebidas.addActionListener(this);
-        combos.jBtnCombo1.addActionListener(this);
+        hamburguesa.jBtnCombo1.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == principal.jBtnCombos) {
-            principal.jPnlMenus.removeAll();
-            principal.jPnlMenus.add(combos);
-            combos.setSize(910, 550);
-            imagenesPanelCombos();
-            principal.jPnlMenus.revalidate();
-            principal.jPnlMenus.repaint();
+            crearCombos();
+            
         }
         if (e.getSource() == principal.jBtnBebidas) {
+            crearBebidas();
+        }
+        
+        if (e.getSource() == hamburguesa.jBtnCombo1) {
+            crearComboIndividual();
+        }
+    }
 
-            principal.jPnlMenus.removeAll();
-            principal.jPnlMenus.repaint();
-            bebidas.setSize(910, 550);
-            imagenesPanelCombos();
-            //cargar el menu de combos
-            principal.jPnlMenus.add(bebidas);
-            principal.jPnlMenus.revalidate();
-            principal.jPnlMenus.repaint();
-        }
-        if (e.getSource() == combos.jBtnCombo1) {
-            principal.jPnlMenus.removeAll();
-            principal.jPnlMenus.add(combosIndividual);
-            combosIndividual.setSize(910, 550);
-            principal.jPnlMenus.revalidate();
-            principal.jPnlMenus.repaint();
-        }
+    public void crearCombos() {
+        principal.jPnlMenus.removeAll();
+        principal.jPnlMenus.add(hamburguesa);
+        imgPanelCombos();
+        hamburguesa.setSize(910, 550);
+        principal.jPnlMenus.revalidate();
+        principal.jPnlMenus.repaint();
     }
 
     public void crearBebidas() {
         principal.jPnlMenus.removeAll();
+        principal.jPnlMenus.add(bebida);
+        imgPanelBebidas();
+        bebida.setSize(910, 550);
+        principal.jPnlMenus.revalidate();
+        principal.jPnlMenus.repaint();
+    }
+
+    public void crearComboIndividual() {
+        principal.jPnlMenus.removeAll();
+        principal.jPnlMenus.add(comboIndividual);
+        imgPanelComboIndividual();
+        comboIndividual.setSize(910, 550);
+        principal.jPnlMenus.revalidate();
         principal.jPnlMenus.repaint();
     }
 
     /*
     *Método para asignar imagenes a cada boton
      */
-    public void imagenes() {
+    public void imgPrincipal() {
 
-        principal.jBtnCombos.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        principal.jBtnBebidas.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\bebidas.png"));
-        principal.jBtnPostres.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\postres.png"));
-        principal.jBtnSnacks.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\snacks.png"));
+        principal.jBtnCombos.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\combo.png"));
+        principal.jBtnBebidas.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Bebidas\\bebidas.png"));
+        principal.jBtnPostres.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Postres\\postres.png"));
+        principal.jBtnSnacks.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Snacks\\snacks.png"));
     }
 
-    public void imagenesPanelCombos() {
-        combos.jBtnCombo1.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        combos.jBtnCombo2.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        combos.jBtnCombo3.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        combos.jBtnCombo4.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        combos.jBtnCombo5.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
-        combos.jBtnCombo6.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\combos.png"));
+    public void imgPanelCombos() {
+        hamburguesa.jBtnCombo1.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h1.png"));
+        hamburguesa.jBtnCombo2.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h2.png"));
+        hamburguesa.jBtnCombo3.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h3.png"));
+        hamburguesa.jBtnCombo4.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h4.png"));
+        hamburguesa.jBtnCombo5.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h5.png"));
+        hamburguesa.jBtnCombo6.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\h6.png"));
     }
-
+    
+    public void imgPanelBebidas(){
+        bebida.jBtnCalientes.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Bebidas\\bebidacaliente.png"));
+        bebida.jBtnFrias.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Bebidas\\bebidas.png"));
+    }
+    
+    public void imgPanelComboIndividual(){
+        comboIndividual.jBtnCombo.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\combo.png"));
+        comboIndividual.jBtnIndividual.setIcon(new ImageIcon(ruta + "\\src\\main\\java\\img\\Hamburguesas\\individual.png"));
+    }
 }
--- src/main/java/vista/jPnlHamburguesas.java
+++ src/main/java/vista/jPnlHamburguesas.java
@@ -9,12 +9,12 @@ package vista;
  *
  * @author javam2019
  */
-public class jIntCombos extends javax.swing.JInternalFrame {
+public class jPnlHamburguesas extends javax.swing.JPanel {
 
     /**
-     * Creates new form jIntCombos
+     * Creates new form jPnlCombos
      */
-    public jIntCombos() {
+    public jPnlHamburguesas() {
         initComponents();
     }
 
@@ -27,18 +27,20 @@ public class jIntCombos extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jBtnCombo1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jBtnCombo4 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
-        jBtnCombo2 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jBtnCombo5 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
         jBtnCombo3 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
+        jBtnCombo1 = new javax.swing.JButton();
+        jBtnCombo2 = new javax.swing.JButton();
         jBtnCombo6 = new javax.swing.JButton();
+        jBtnCombo4 = new javax.swing.JButton();
+        jBtnCombo5 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+
+        setPreferredSize(new java.awt.Dimension(910, 550));
 
         jBtnCombo1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -49,29 +51,29 @@ public class jIntCombos extends javax.swing.JInternalFrame {
         jLabel1.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         jLabel1.setText("Combo 1");
 
-        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        jLabel3.setText("Combo 4");
-
         jLabel2.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         jLabel2.setText("Combo 2");
 
-        jLabel5.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        jLabel5.setText("Combo 5");
+        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel3.setText("Combo 4");
 
         jLabel4.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         jLabel4.setText("Combo 3");
 
+        jLabel5.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel5.setText("Combo 5");
+
         jLabel6.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         jLabel6.setText("Combo 6");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(146, 146, 146)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(152, 152, 152)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jBtnCombo1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jBtnCombo4, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -84,25 +86,25 @@ public class jIntCombos extends javax.swing.JInternalFrame {
                             .addComponent(jBtnCombo3, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jBtnCombo6, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(38, 38, 38)
+                        .addGap(190, 190, 190)
                         .addComponent(jLabel1)
                         .addGap(173, 173, 173)
                         .addComponent(jLabel2)
                         .addGap(175, 175, 175)
                         .addComponent(jLabel4))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
+                        .addGap(193, 193, 193)
                         .addComponent(jLabel3)
                         .addGap(173, 173, 173)
                         .addComponent(jLabel5)
                         .addGap(171, 171, 171)
                         .addComponent(jLabel6)))
-                .addContainerGap(157, Short.MAX_VALUE))
+                .addContainerGap(153, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(63, 63, 63)
+                .addGap(101, 101, 101)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jBtnCombo1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -122,10 +124,8 @@ public class jIntCombos extends javax.swing.JInternalFrame {
                     .addComponent(jLabel3)
                     .addComponent(jLabel5)
                     .addComponent(jLabel6))
-                .addContainerGap(112, Short.MAX_VALUE))
+                .addContainerGap(96, Short.MAX_VALUE))
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBtnCombo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnCombo1ActionPerformed
