bae6788433a15f8d538ee87710c770d9502da9eb
Dudu0101 <Duinles_urz@outlook.es>
2019-08-20 16:53:22 -0600

    Cambios
    Cambios

--- src/main/java/controlador/CarritoControlador.java
+++ src/main/java/controlador/CarritoControlador.java
@@ -5,10 +5,38 @@
  */
 package controlador;
 
-/**
- *
- * @author futureBurguer
- */
+import static controlador.ProductoSeleccionado.agregarCarrito;
+import modelo.DetallePedido;
+import vista.JintCarrito;
+import vista.JintCliente;
+
 public class CarritoControlador {
-    
+
+    //Frames a utlizar
+    JintCarrito carrito = new JintCarrito();
+    JintCliente cliente = new JintCliente();
+
+    //Controladores a utlizar
+    ClienteControlador controladorCliente;
+
+    //Modelos a utilizar
+    DetallePedido orden = new DetallePedido();
+
+    private byte pedido_id=1;
+
+    public CarritoControlador(JintCarrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public void agregarDetallePedido() {
+        pedido_id++;
+        for (int i = 0; i < agregarCarrito.size() - 1; i++) {
+            orden.setPedido_id(pedido_id);
+            orden.setProducto_tamaño_id(agregarCarrito.get(i).getProducto_tamaño_id());
+            orden.setCantidad(agregarCarrito.get(i).getCantidad());
+            orden.setPrecio(agregarCarrito.get(i).getPrecio());            
+        }
+        
+    }
+
 }
